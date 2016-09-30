390fc092b883e440e65d1a5de887b4b63882a69e
Jordan Hamade <ardf2014@hotmail.com>
2016-09-30 16:31:47 -0700

    bug fixed with 9
    Signed-off-by: Jordan Hamade <ardf2014@hotmail.com>

--- application/hooks/MyHooks.php
+++ application/hooks/MyHooks.php
@@ -2,11 +2,7 @@
 class MyHook{
   function filterData(){
     /* References the CI superobject to have access to the webpage. */
-          //  $this->CI =& get_instance();
-            //$page = $this->CI->output->get_output();
             $CI = & get_instance();
-            //var_dump($CI);
-            //die();
             $page = $CI->output->get_output();
             /* Creates a new DOMDocument and loads the HTML of the webpage. */
             $dom  = new DOMDocument();
@@ -17,19 +13,17 @@ class MyHook{
                 /* If the class of the element is lead, it is modified. */
                 if($element->getAttribute('class') == 'lead')
                 {
-                    //$element->textContent = "hello";
                     $temp = $element->textContent;
                     $words = explode(' ', $temp);
-                    $alter = array();
+                    $inc = 0;
                     foreach($words as $word){
-                      if(preg_match('/^([a-z]{4})$/i', $word)){
-                        array_push($alter, $word);
+                      if(preg_match('/^[a-z]{4}$/i', $word)){
+                        $words[$inc] = "****";
                       }
+                      $inc++;
                     }
-                    foreach($alter as $t){
-                      $s = str_replace($alter,"****", $element->textContent);
-                      $element->textContent = $s;
-                    }
+                    $fin = implode(" ", $words);
+                    $element->textContent = $fin;
                 }
             }
             echo $dom->saveHTML();
